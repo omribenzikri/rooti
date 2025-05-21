e7fab229e5dbddbf62df973b2179a2a41591397d
Omri Ben Zikri <omribenzikri10@gmail.com>
2025-05-21 17:46:07 +0000

    Calculating array size at compile with the handy ARRAY_SIZE macro

--- rooti.c
+++ rooti.c
@@ -11,14 +11,13 @@ MODULE_VERSION("1.0.0");
 static asmlinkage long (*orig_mkdir)(const struct pt_regs *regs);
 
 static asmlinkage long hook_mkdir(const struct pt_regs *regs) {
-    printk(KERN_INFO "Hooked mkdir! :D\n");
+    printk(KERN_INFO "rooti: hooked mkdir! :D\n");
     orig_mkdir(regs);
     return 0;
 }
 
-
 // List of system calls to hook :D
-struct rooti_ftrace_hook hooks[] = {
+struct rooti_syscall_hook hooks[] = {
     ROOTI_HOOK("sys_mkdir", hook_mkdir, &orig_mkdir)
 };
 
@@ -35,7 +34,7 @@ static int __init rooti_init(void)
     }
 
     // Install hooks :D
-    ret = rooti_install_hooks(hooks, 1);
+    ret = rooti_install_hooks(hooks, ARRAY_SIZE(hooks));
     if (ret < 0) {
         printk(KERN_DEBUG "rooti: rooti_install_hooks() failed: %d\n", ret);
         return ret;
@@ -48,7 +47,7 @@ static int __init rooti_init(void)
 static void __exit rooti_exit(void)
 {
     printk(KERN_INFO "rooti: exit\n");
-    rooti_uninstall_hooks(hooks, 1);
+    rooti_uninstall_hooks(hooks, ARRAY_SIZE(hooks));
 }
 
 module_init(rooti_init);
