088a6891e05ca0a8c5e2d079fb1b45af79270f9c
Omri Ben Zikri <omribenzikri10@gmail.com>
2025-06-17 10:47:43 +0000

    Moved user configuration into separate file

---
+++ src/config.h
@@ -0,0 +1,8 @@
+// Prefix of files that we wish to hide
+#define ROOTI_HIDE_PREFIX "secret"
+
+// User to hide
+#define ROOTI_HIDE_USER "omri"
+
+// Port number to hide from tools like netstat
+#define ROOTI_HIDE_PORT 8080
